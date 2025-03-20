33d0435ebddf9066cae857767c12cd3d3e6b9012
Oilivur <oilivur@protonmail.com>
2025-03-20 23:27:20 +0200

    timmin urli

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Admin from "@/views/Admin.vue";
 import Termas from "@/views/Termas.vue";
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
