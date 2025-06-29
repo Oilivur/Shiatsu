62971c9672e182fa645c0a26ca9b73bdfe18102c
Oilivur <oilivur@protonmail.com>
2025-06-29 15:52:03 +0300

    removed hash history

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Admin from "@/views/Admin.vue";
 import Termas from "@/views/Termas.vue";
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/Shiatsu/"),
+  history: createWebHistory("/"),
   routes,
 });
 
